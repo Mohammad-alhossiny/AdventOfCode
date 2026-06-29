df81b85a151c6d00721e746e944b7c64fe735050
mohammad Alhossiny Alshamy <mohammadalhossinyvs@gmail.com>
2026-06-29 15:01:06 +0300

    2016 day9 p2

--- 2016py/Day9.py
+++ 2016py/Day9.py
@@ -16,11 +16,39 @@ def main(in_string:str)->int:
             cursor += 1
     return len_in_str
 
+# part2, AI assisted because I'm lazy:
+def decompressed_length(s, start, end):
+    cursor = start
+    total = 0
+
+    while cursor < end:
+        if s[cursor] != "(":
+            total += 1
+            cursor += 1
+        else:
+            right = s.find(")", cursor)
+            c, r = map(int, s[cursor + 1:right].split("x"))
+
+            # Recursively compute the length of the next c characters
+            total += r * decompressed_length(
+                s,
+                right + 1,
+                right + 1 + c
+            )
+
+            # Skip over those c characters
+            cursor = right + 1 + c
+
+    return total
+
+def main2(in_string):
+    return decompressed_length(in_string, 0, len(in_string))
+
 if __name__ == '__main__':
-    test_lines1 = ["ADVENT", "A(1x5)BC", "(3x3)XYZ", "A(2x2)BCD(2x2)EFG", "(6x1)(1x3)A", "X(8x2)(3x3)ABCY"]
+    test_lines = ["ADVENT", "A(1x5)BC", "(3x3)XYZ", "A(2x2)BCD(2x2)EFG", "(6x1)(1x3)A", "X(8x2)(3x3)ABCY"]
 
     with open("inputs/Day9.txt") as file:
         in_lines = file.read()
 
     line = list(map(str.strip, in_lines.strip().splitlines()))[0]
-    print(main(line))
+    print(main2(line))
